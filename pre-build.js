be105ed26d97ea856b9c5942aebcb85f8c304fff
colorhook <colorhook@gmail.com>
2013-11-20 13:38:17 +0800

    add windows imple

---
+++ pre-build.js
@@ -0,0 +1,33 @@
+'use strict';
+var fs = require('fs');
+var request = require('request');
+var chalk = require('chalk');
+var Mocha = require('mocha');
+var mocha = new Mocha({ui: 'bdd', reporter: 'min'});
+var advpng = require('./lib/advpng');
+var binPath = advpng.path;
+var binUrl = advpng.url;
+
+function runTest() {
+	mocha.addFile('test/test-path.js');
+	mocha.run(function (failures) {
+		if (failures > 0) {
+			console.log(chalk.red('pre-build test failed, compiling from source...'));
+            throw new Error("node-advpng cannot be installed, please contact colorhook@gmail.com");
+		} else {
+			console.log(chalk.green('pre-build test passed successfully, skipping build...'));
+		}
+	});
+}
+
+console.log(binPath);
+if (fs.existsSync(binPath)) {
+	runTest();
+} else {
+	request.get(binUrl)
+		.pipe(fs.createWriteStream(binPath))
+		.on('close', function () {
+			fs.chmod(binPath, '0755');
+			runTest();
+		});
+}
---
+++ test/test-path.js
@@ -0,0 +1,41 @@
+'use strict';
+var fs = require('fs');
+var path = require('path');
+var execFile = require('child_process').execFile;
+var assert = require('assert');
+
+describe('advpng', function () {
+
+	after(function () {
+        try{
+		 fs.unlinkSync('test/example.adv.png');
+        }catch(err){}
+	});
+
+
+    it('get the advpng version', function (cb) {
+		var binPath = require('../lib/advpng.js').path;
+		execFile(binPath, ['--version'], function (err, stdout, stderr) {
+            console.log(stdout.toString().toLowerCase())
+			assert(stdout.toString().toLowerCase().indexOf('advancecomp') !== -1);
+			cb();
+		});
+	});
+
+
+	it('should minify a .png', function (cb) {
+		var binPath = require('../lib/advpng.js').path;
+        var minFile = path.join(__dirname, 'example.min.png');
+        var advFile = path.join(__dirname, 'example.adv.png');
+        var data = fs.readFileSync(minFile);
+        fs.writeFileSync(advFile, data);
+		var args = [advFile, '-z', '-4'];
+		execFile(binPath, args, function () {
+			var actual = fs.statSync(advFile).size;
+			var original = fs.statSync(minFile).size;
+            console.log("actual:", actual, " original:", original);
+			assert(actual < original);
+			cb();
+		});
+	});
+});
